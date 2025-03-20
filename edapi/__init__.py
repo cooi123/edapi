8d1c5678628220aa7739564a9bd14e32125ec478
cooi123 <calebooi@hotmail.com>
2025-03-21 02:06:00 +1100

    refactor: update example

--- edapi/__init__.py
+++ edapi/__init__.py
@@ -4,6 +4,7 @@ Package for Ed API Python integration.
 
 # the only default import should be the API class
 from .edapi import EdAPI
-from .models.user import User, UserCourse, CourseInfo
+from .models.user import User
+from .models.course import CourseInfo
 
-__all__ = ["EdAPI", "User", "UserCourse", "CourseInfo"]
+__all__ = ["EdAPI", "User", "CourseInfo", "CourseInfo"]
--- examples/getuserinfo.py
+++ examples/getuserinfo.py
@@ -3,11 +3,16 @@ from edapi import EdAPI
 from edapi.models.user import User
 # load from environment variables
 from dotenv import load_dotenv
+import pandas as pd
 load_dotenv()
 
 ed = EdAPI(os.getenv("ED_API_TOKEN"))
 
-user_info = ed.get_user_info()
-user: User = user_info.get_user_info_summary()
-
+user:User = ed.get_user_info()
+active_courses = user.get_active_courses()
 print(f"Hello {user.name}!")
+#convert active courses into pandas df
+active_courses_df = pd.DataFrame([course.to_dict() for course in active_courses])
+print(active_courses_df)
+
+
